91bc55d654da50e4cc620334880ea35f3e83ccf4
Chris Laskey <contact@chrislaskey.com>
2022-01-25 22:11:36 -0500

    Add tmux to brew install list

--- brew-install.sh
+++ brew-install.sh
@@ -1,5 +1,5 @@
 ### General packages
-brew install ack ctags diff-so-fancy gpg ffmpeg fpp gettext gnu-sed imagemagick puma/puma/puma-dev rename ssh-copy-id vim wget xz
+brew install ack ctags diff-so-fancy gpg ffmpeg fpp gettext gnu-sed imagemagick puma/puma/puma-dev rename ssh-copy-id tmux vim wget xz
 
 ### General packages - Casks
 brew cask install chromedriver
