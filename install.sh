5165fc4b859a0c692ebe8aa9189157cc27e47bda
Chris Laskey <contact@chrislaskey.com>
2026-07-07 18:23:15 -0400

    Basic installation documentation/script

--- install.sh
+++ install.sh
@@ -1,26 +1,47 @@
 #!/bin/bash
 
-# 1. Install modern bash (macOS ships bash 3.2 from 2007 due to GPLv3 licensing):
+# Install homebrew
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# Install modern bash (macOS ships with version from 2007)
 
 brew install bash
 
-# 2. Add the new bash to allowed shells:
+# Add the new bash to allowed shells
 
 echo '/opt/homebrew/bin/bash' | sudo tee -a /etc/shells
 
-# 3. Set it as your default shell:
+# Set it as your default shell
 
 chsh -s /opt/homebrew/bin/bash
 
-# 4. Symlink these dotfiles into place:
+# Symlink these dotfiles into place
 
-ln -sf ~/Desktop/dotfiles/.bash_profile ~/.bash_profile
-ln -sf ~/Desktop/dotfiles/.bashrc ~/.bashrc
+ln -sf ./.bash_profile ~/.bash_profile
+ln -sf ./.bashrc ~/.bashrc
+ln -sf ./.config ~/.config
+ln -sf ./.hammerspoon ~/.hammerspoon
+ln -sf ./.tmux.conf ~/.tmux.conf
+ln -sf ./.zshrc ~/.zshrc
 
-# 5. Open a new terminal session. Verify with:
+# Create ~/.secrets for secret values in env vars (ex: API keys)
 
-echo $BASH_VERSION   # Should show 5.x
-
-# 6. Move your API keys out of this file and into ~/.secrets:
-
+touch ~/.secrets
 chmod 600 ~/.secrets
+
+# Create new bash instance
+
+/bin/bash
+
+# Install the rest of the brew packages
+
+brew bundle --file=./Brewfile
+
+# Install asdf plugins
+
+asdf plugin add python https://github.com/asdf-community/asdf-python.git
+asdf plugin add erlang https://github.com/asdf-vm/asdf-erlang.git
+asdf plugin add elixir https://github.com/asdf-vm/asdf-elixir.git
+asdf plugin add dexter https://github.com/remoteoss/dexter.git && asdf install dexter latest && asdf set --home dexter latest
+asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
